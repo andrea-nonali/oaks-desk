eb9bf593e308eb0d2733fb819210018535345e31
Andrea Nonali <nonali.andrea@gmail.com>
2021-10-30 23:30:34 +0200

    refactor: move class -> viewsets in custom folder

--- pokemonAPI/urls.py
+++ pokemonAPI/urls.py
@@ -1,15 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
 
-import coreAPI.views.group_view_set
-import coreAPI.views.pokemon_view_set
-import coreAPI.views.user_view_set
+import coreAPI.views.view_sets.group_view_set
+import coreAPI.views.view_sets.pokemon_view_set
+import coreAPI.views.view_sets.user_view_set
 from coreAPI.views.pokemon_insights import *
 
 router = routers.DefaultRouter()
-router.register(r'users', coreAPI.views.user_view_set.UserViewSet)
-router.register(r'groups', coreAPI.views.group_view_set.GroupViewSet)
-router.register(r'pokemons', coreAPI.views.pokemon_view_set.PokemonViewSet)
+router.register(r'users', coreAPI.views.view_sets.user_view_set.UserViewSet)
+router.register(r'groups', coreAPI.views.view_sets.group_view_set.GroupViewSet)
+router.register(r'pokemons', coreAPI.views.view_sets.pokemon_view_set.PokemonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
