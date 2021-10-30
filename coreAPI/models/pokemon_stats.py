8df0deb734071f2b2bcf33a4773ce284695b7a0e
Andrea Nonali <nonali.andrea@gmail.com>
2021-10-31 00:11:05 +0200

    get_best_base_total_stats_pokemon_for_generation no consider pokemon status

--- coreAPI/models/pokemon_stats.py
+++ coreAPI/models/pokemon_stats.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models import Q
 
+from coreAPI.models.pokemon import POKEMON_STATUS
+
 
 class PokemonStats(models.Model):
     pokemon = models.OneToOneField('Pokemon', null=False, on_delete=models.CASCADE, related_name='stats')
@@ -13,9 +15,22 @@ class PokemonStats(models.Model):
     total_points = models.PositiveSmallIntegerField()
 
     @staticmethod
-    def get_best_base_total_stats_pokemon_for_generation(generation: str) -> list:
+    def get_best_base_total_stats_pokemon_for_generation(
+            generation: str,
+            include_legendaries=True,
+            include_mythicals=True,
+            include_mega_evolutions=True
+    ) -> list:
+        available_statuses = [POKEMON_STATUS['normal'], POKEMON_STATUS['sub_legendary']]
+        if include_legendaries:
+            available_statuses.append(include_legendaries)
+        if include_mythicals:
+            available_statuses.append(include_mythicals)
+        if include_mega_evolutions:
+            available_statuses.append(include_mega_evolutions)
+
         return list(
-            PokemonStats.objects.filter(pokemon__generation=generation)
+            PokemonStats.objects.filter(pokemon__generation=generation, pokemon__status__in=available_statuses)
             .order_by('-total_points')
         )
 
