69a29162d40c891c791553d1557217a4387ff866
Andrea Nonali <nonali.andrea@gmail.com>
2021-10-31 20:25:32 +0100

    invert logic of legendaries

--- coreAPI/views/pokemon_stats_view.py
+++ coreAPI/views/pokemon_stats_view.py
@@ -20,8 +20,8 @@ class BestPokemonAPIView(APIView):
     def get(self, request):
         generation = self.request.GET.get('generation')
         type = self.request.GET.get('type')
-        include_legendaries = True if self.request.GET.get('include_legendaries') == 'True' else False
-        include_mythicals = True if self.request.GET.get('include_mythicals') == 'True' else False
+        include_legendaries = False if self.request.GET.get('include_legendaries') == 'False' else True
+        include_mythicals = False if self.request.GET.get('include_mythicals') == 'False' else True
 
         pokemon_stats = PokemonStats.get_best_base_total_stats_pokemon_for(
             generation,
