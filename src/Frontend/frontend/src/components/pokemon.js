9e4e87a0fa06254a64413dd0f542f87585396273
Andrea Nonali <nonali.andrea@gmail.com>
2021-11-14 18:19:36 +0100

    restyle a little bit

--- src/Frontend/frontend/src/components/pokemon.js
+++ src/Frontend/frontend/src/components/pokemon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function Pokemon() {
     const [pokemonList, setpokemonList] = useState([{}])
@@ -15,14 +16,19 @@ export default function Pokemon() {
         <div className="Pokemon">
           <header className="App-header">
             <div className=" border-2">
-                <h1 className="flex-auto text-xl font-bold">Lista di Pokemon</h1>
+                <h1 className="flex-auto text-xl font-medium">Lista di Pokemon</h1>
             </div>
             <div>
                 {
-                    pokemonList.map((pokemon) => {
+                    pokemonList.map( pokemon => {
                         return (
                             <div>
-                                <p>{pokemon.name}</p>
+                                <Link className="flex-auto text-s"
+                                    to={`/pokemons/${pokemon.id}`}
+                                    key={pokemon.id}
+                                >
+                                    { pokemon.name }
+                                </Link>
                                 <br/>
                             </div>
                         )
