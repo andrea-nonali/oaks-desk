18916fd3b70819184cc1c53aef9376e0c3fdd48f
Andrea Nonali <nonali.andrea@gmail.com>
2021-11-14 14:12:05 +0100

    initial setup: react + tailwind css

---
+++ src/Frontend/frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class=" border-2">
+          <h1>Hello Tailwind</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
