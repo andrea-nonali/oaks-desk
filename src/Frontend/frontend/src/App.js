4e9d29c36e017dc88ef383b72c820d60359db3bd
Andrea Nonali <nonali.andrea@gmail.com>
2021-11-14 19:42:27 +0100

    rename file

--- src/Frontend/frontend/src/App.js
+++ src/Frontend/frontend/src/App.js
@@ -11,8 +11,7 @@ export default function App() {
       </header>
 
       <nav>
-        <Link to="/pokemon-list">Pokemon List</Link> |{" "}
-      </nav>
+        <Link to="/pokemon-list">Pokemon List</Link></nav>
     </div>
   );
 }
---
+++ src/Frontend/frontend/src/components/PokemonList.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+export default function PokemonList() {
+    const [pokemonList, setpokemonList] = useState([{}])
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/get-all-pokemons')
+        .then(response => {
+            setpokemonList(response.data)
+        })
+    }, [])
+    
+    return (
+        <div>
+          <header className="App-header">
+            <div className=" border-2">
+                <h1 className="flex-auto text-xl font-medium">Lista di Pokemon</h1>
+            </div>
+            <div>
+                {
+                    pokemonList.map( pokemon => {
+                        return (
+                            <div key={pokemon.id}>
+                                <Link className="flex-auto text-s"
+                                    to={`/pokemon/${pokemon.id}`}
+                                >
+                                    { pokemon.name }
+                                </Link>
+                                <br/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+          </header>
+        </div>
+      );
+}
