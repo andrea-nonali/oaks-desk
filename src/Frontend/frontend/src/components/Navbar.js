b026acc67cb6fb807b9e7c56f5acb03ebd10e17f
Andrea Nonali <nonali.andrea@gmail.com>
2021-11-15 11:50:43 +0100

    use same css class for every navbar item

--- src/Frontend/frontend/src/components/Navbar.js
+++ src/Frontend/frontend/src/components/Navbar.js
@@ -5,14 +5,14 @@ export default function Navbar() {
     <div className="Navbar">
         <nav className="bg-white shadow">
           <ul className="flex">
-          <li className="mr-3 p-4">
-            <a className="inline-block" href="/">
-                Home
-            </a>
-          </li>
-          <li className="mr-3 p-4">
-            <Link to="/pokemon-list">Pokemon List</Link>
-          </li>
+            <li className="nav-item">
+                <a className="inline-block" href="/">
+                    Home
+                </a>
+            </li>
+            <li className="nav-item">
+                <Link to="/pokemon-list">Pokemon List</Link>
+            </li>
         </ul>
         </nav>
     </div>
