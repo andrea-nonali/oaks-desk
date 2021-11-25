28f0b2443c9889623e6c5105ed553d54223d6a37
Andrea Nonali <nonali.andrea@gmail.com>
2021-11-25 23:21:48 +0100

    Added more informations to the pokemon detail

--- src/Frontend/frontend/src/components/pokemon.js
+++ src/Frontend/frontend/src/components/pokemon.js
@@ -5,12 +5,22 @@ import Navbar from './Navbar';
 
 export default function Pokemon() {
   let params = useParams();
-  const [pokemonData, setPokemonData] = useState({})
+  const [pokemon, setPokemon] = useState({})
+  const [pokemonAbilities, setPokemonAbilities] = useState({})
+  const [pokemonStats, setPokemonStats] = useState({})
 
   useEffect(() => {
-    axios.get(`http://127.0.0.1:8000/api/get-pokemon-data?pokemon-id=${params.pokemonId}`)
+    axios.get(`http://127.0.0.1:8000/api/get-pokemon-data?pokemon_id=${params.pokemonId}`)
     .then(response => {
-        setPokemonData(response.data)
+        setPokemon(response.data)
+    })
+    axios.get(`http://127.0.0.1:8000/api/get-abilities?pokemon_id=${params.pokemonId}`)
+    .then(response => {
+        setPokemonAbilities(response.data)
+    })
+    axios.get(`http://127.0.0.1:8000/api/get-stats?pokemon_id=${params.pokemonId}`)
+    .then(response => {
+        setPokemonStats(response.data)
     })
   }, []);
 
@@ -20,17 +30,33 @@ export default function Pokemon() {
               <Navbar />
       </header>
       <div className="flex">
-        <div className="w-1/6 h-screen">
-          <img class="w-30 h-30 rounded-full m-2 p-2" src="/blank-profile.png" alt="" width="200" height="200"/>
+        <div className="w1-/6 h-screen">
+          <img className="w-30 h-30 rounded-full m-2 p-2" src="/blank-profile.png" alt="" width="200" height="200"/>
         </div>
         <div className="h-screen w-5/6 ml-3">
-          <h1 className="mt-3 text-5xl font-extrabold">{pokemonData.name}</h1>
-          <p className="font-extralight mt-2">Generation {pokemonData.generation}</p>
+          <h1 className="mt-3 text-5xl font-extrabold">{pokemon.name}</h1>
+          <p className="font-extralight mt-2">Generation {pokemon.generation}</p>
           <div className="mt-3">
             <h3 className="text-3xl">Type</h3>
             <div className="flex">
-              <p>{pokemonData.type_1}</p>
-              <p className="ml-3">{pokemonData.type_2}</p>
+              <p>{pokemon.type_1}</p>
+              <p className="ml-3">{pokemon.type_2}</p>
+            </div>
+            <h3 className="text-3xl mt-5">Abilities</h3>
+            <div className="flex">
+              <p>{pokemonAbilities.ability_1}</p>
+              <p className="ml-3">{pokemonAbilities.ability_2}</p>
+              <p className="ml-3">{pokemonAbilities.ability_hidden}</p>
+            </div>
+            <h3 className="text-3xl mt-5">Stats</h3>
+            <div>
+              <p>HP: {pokemonStats.hp}</p>
+              <p>Attack: {pokemonStats.attack}</p>
+              <p>Special Attack: {pokemonStats.sp_attack}</p>
+              <p>Defense: {pokemonStats.defense}</p>
+              <p>Special Defense: {pokemonStats.sp_defense}</p>
+              <p>Speed: {pokemonStats.speed}</p>
+              <p>Total: {pokemonStats.total_points}</p>
             </div>
           </div>
         </div>
